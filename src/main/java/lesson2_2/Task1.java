de9c5afa3a50f10a192c4f7c53d9afd8c1e87c5c
cyberparker <crap75@mail.ru>
2025-05-22 21:49:41 +0300

    initialCommit

---
+++ src/main/java/lesson2_2/Task1.java
@@ -0,0 +1,16 @@
+package lesson2_2;
+
+public class Task1 {
+
+    /*
+    Создайте метод printThreeWords(), который при вызове    должен отпечатать в столбец три слова: Orange, Banana, Apple     */
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void main(String[] args){
+        printThreeWords();
+
+    }
+}
